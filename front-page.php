263d4b5392de478fe957c9069bc3865891c37001
Matt <matthewtroup@outlook.com>
2019-06-20 19:11:17 -0400

    testing arrow section from production

--- front-page.php
+++ front-page.php
@@ -51,7 +51,42 @@
                 
                 <p>True North Asset Group provides property management services throughout the Black Hills. We can help you get the most return on your investment properties and take care of the day-to-day operations so you don’t have to. With our integrated approach, our team can manage and lease your properties and keep your tenants happy.</p>
                 
-                <p class="text-center">Arrow section with red boxes - plugin/dynamic</p>
+                <!-- START OF ARROW SECTON -->
+                
+                
+                    <div class="smile_icon_list_wrap ult_info_list_container ult-adjust-bottom-margin   info_list">
+                                                <ul class="smile_icon_list top square with_bg">
+                                                    <li class="icon_list_item" style=" font-size:72px;">
+                                                        <div class="icon_list_icon" data-animation="" data-animation-delay="03" style="font-size:24px;border-width:1px;border-style:none;background:#d1083e;color:#ffffff;border-color:#333333;"><i class="Defaults-arrow-right"></i></div>
+                                                        <div class="icon_description" id="Info-list-wrap-3259" style="font-size:24px;">
+                                                            <h3 class="ult-responsive info-list-heading" data-ultimate-target='#Info-list-wrap-3259 h3' data-responsive-json-new='{"font-size":"desktop:16px;","line-height":"desktop:24px;"}' style="">Tenant Access</h3>
+                                                            <div class="icon_description_text ult-responsive" data-ultimate-target='#Info-list-wrap-3259 .icon_description_text' data-responsive-json-new='{"font-size":"desktop:13px;","line-height":"desktop:18px;"}' style=""></div>
+                                                        </div>
+                                                        <div class="icon_list_connector" data-animation="fadeInLeft" data-animation-delay="03" style="border-top-width: 3px;border-top-style: dashed;border-color: #333333;top: 35.5px;"></div>
+                                                        <a class="ulimate-info-list-link" href="https://tnorth.twa.rentmanager.com/"></a>
+                                                    </li>
+                                                    <li class="icon_list_item" style=" font-size:72px;">
+                                                        <div class="icon_list_icon" data-animation="" data-animation-delay="03" style="font-size:24px;border-width:1px;border-style:none;background:#d1083e;color:#ffffff;border-color:#333333;"><i class="Defaults-arrow-right"></i></div>
+                                                        <div class="icon_description" id="Info-list-wrap-5743" style="font-size:24px;">
+                                                            <h3 class="ult-responsive info-list-heading" data-ultimate-target='#Info-list-wrap-5743 h3' data-responsive-json-new='{"font-size":"desktop:16px;","line-height":"desktop:24px;"}' style="">Tenant Application</h3>
+                                                            <div class="icon_description_text ult-responsive" data-ultimate-target='#Info-list-wrap-5743 .icon_description_text' data-responsive-json-new='{"font-size":"desktop:13px;","line-height":"desktop:18px;"}' style=""></div>
+                                                        </div>
+                                                        <div class="icon_list_connector" data-animation="fadeInLeft" data-animation-delay="03" style="border-top-width: 3px;border-top-style: dashed;border-color: #333333;top: 35.5px;"></div>
+                                                        <a class="ulimate-info-list-link" href="https://tnorth.twa.rentmanager.com/applynow"></a>
+                                                    </li>
+                                                    <li class="icon_list_item" style=" font-size:72px;">
+                                                        <div class="icon_list_icon" data-animation="" data-animation-delay="03" style="font-size:24px;border-width:1px;border-style:none;background:#d1083e;color:#ffffff;border-color:#333333;"><i class="Defaults-arrow-right"></i></div>
+                                                        <div class="icon_description" id="Info-list-wrap-3397" style="font-size:24px;">
+                                                            <h3 class="ult-responsive info-list-heading" data-ultimate-target='#Info-list-wrap-3397 h3' data-responsive-json-new='{"font-size":"desktop:16px;","line-height":"desktop:24px;"}' style="">Property Owner Access</h3>
+                                                            <div class="icon_description_text ult-responsive" data-ultimate-target='#Info-list-wrap-3397 .icon_description_text' data-responsive-json-new='{"font-size":"desktop:13px;","line-height":"desktop:18px;"}' style=""></div>
+                                                        </div>
+                                                        <div class="icon_list_connector" data-animation="fadeInLeft" data-animation-delay="03" style="border-top-width: 3px;border-top-style: dashed;border-color: #333333;top: 35.5px;"></div>
+                                                        <a class="ulimate-info-list-link" href="https://tnorth.owa.rentmanager.com/"></a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                
+                <!-- END OF ARROW SECTION -->
                 
                 <h2>Request a free management proposal or accounting report sample.</h2>
                 <p class="text-center"><button type="button" class="btn btn-red">Contact Us Today!</button></p>
