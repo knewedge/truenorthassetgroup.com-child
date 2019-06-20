f1b151e40c566dc36521ece64be1bb357c24009b
Matt <matthewtroup@outlook.com>
2019-06-20 18:26:05 -0400

    added footer file to project, moved code.

---
+++ footer.php
@@ -0,0 +1,42 @@
+<footer>
+    <div class="container">
+        <div class="row footer-content">
+            <div class="col-md-4">
+                <h3>ABOUT</h3>
+                <p>True North Asset Group is Rapid City’s newest commercial real estate team from Keller Williams Realty Black Hills. READ MORE .</p>
+             
+                    <h3>Get Connected</h3>
+                <p>
+                    <a class="footer-icons" href="#"><i class="fa fa-facebook fa-fw"></i></a>
+                    <a class="footer-icons" href="#"><i class="fa fa-twitter fa-fw"></i></a>
+                    <a class="footer-icons" href="#"><i class="fa fa-linkedin fa-fw"></i></a>
+                    <a class="footer-icons" href="#"><i class="fa fa-youtube fa-fw"></i></a>
+                    <a class="footer-icons" href="#"><i class="fa fa-instagram fa-fw"></i></a>
+                
+                </p>
+            </div>
+            <div class="col-md-3">
+                <h3>CONTACT</h3>
+                <p><i class="fa fa-building-o"></i> Keller Williams Realty Black Hills 2401 W Main Street Rapid City, SD 57702</p>
+                <p><i class="fa fa-phone"></i> (605) 343-7500</p>
+                <p><i class="fa fa-envelope-o"></i> contact@truenorthassetgroup.com</p>
+                <p><i class="fa fa-desktop"></i> Visit Us at Our Office: 1315 Haines Avenue, Suite C Rapid City, SD 57701</p>
+            </div>
+            <div class="col-md-2">
+                <h3>LATEST LISTINGS</h3>
+                <p>listing - dyanmic</p>
+                <p>listing - dyanmic</p>
+                <p>listing - dyanmic</p>
+            </div>
+            <div class="col-md-3">
+                <h3>OUR NEWSLETTER</h3>
+                <div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="search" aria-label="search" aria-describedby="basic-addon2">
+  <div class="input-group-append">
+    <span class="input-group-text" id="basic-addon2">GO</span>
+  </div>
+</div>
+            </div>
+        </div>
+    </div>
+</footer>
