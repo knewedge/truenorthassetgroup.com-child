18a5cacf569dcc3746c0305ebf8aa402993cec46
Nick Eldred <knewedge@gmail.com>
2019-06-25 23:38:37 -0500

    changed footer to footer.php in single-listing.php

--- single-listings.php
+++ single-listings.php
@@ -110,33 +110,12 @@
 	<div class="col-md-6"><?php dynamic_sidebar('getintouch-section'); ?></div>
 	<div id="contact-second-col" class="col-md-6"><?php dynamic_sidebar('connected-section'); ?></div>
 </div>
-<?php //get_footer(); ?>
- <!-- FOOTER -->
- <footer id="footer" class="<?php echo czr_fn__f('tc_footer_classes', '') ?>">
-  	<?php //do_action( '__footer' ); // hook of footer widget and colophon?>
-	<div class="row">
-		<div class="col-md-6">
-			<p>&copy; <?php echo date('Y'); ?></span> <a href="#">Keller Williams Realty Black Hills</a> &ndash; All rights reserved</p>
-		</div>
-		<!--Removed
-		<div class="col-md-6">
-			<p>Listings provided by Keller Williams Realty Black Hills
-		</div>
-		-->
-	</div>
-</footer>
+<?php include "footer.php"; ?>
 <?php
 do_action( '__after_page_wrap' );
 wp_footer(); //do not remove, used by the theme and many plugins
 do_action( '__after_footer' );
 ?>
-<script>
-/*
-jQuery(function(){
-	jQuery('.gallery').slick();
-});
-*/
-</script>
 </body>
 <?php do_action( '__after_body' ); ?>
 </html>
