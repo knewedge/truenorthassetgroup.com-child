0dd117a9de2a949fd2d01caacb685715bedb6824
Nick Eldred <knewedge@gmail.com>
2019-07-08 00:11:20 -0500

    removed old code and turned contact us today into anchor

--- front-page.php
+++ front-page.php
@@ -63,51 +63,13 @@
 					<!-- END OF ARROW SECTION -->
 					
 					<h2>Request a free management proposal or accounting report sample.</h2>
-					<p class="text-center"><button type="button" class="btn btn-red">Contact Us Today!</button></p>
+					<p class="text-center"><a type="button" class="btn btn-red">Contact Us Today!</a></p>
 					
 				</div>
 			</div>
 		</div>
     </section>
-<?php get_footer(); ?>
-
-<?php /*
-<a name="services"></a>
-
-<?php dynamic_sidebar('services-section'); ?>
-
-<div id="main-wrapper" class="section">
-	<div class="featured-section-wrap">
-		<h3 class="center-h3" style="color:#ffffff!important;">Featured Listings</h3>
-		<?php do_action('__before_main_container'); ?>
-		<div style="text-align:center;">
-			<a id="see-all-link" href="/rapidcity/search">See all listings >></a>
-		</div>
-	</div>
-</div>
-<div id="contact-section" class="row">
-	<div class="col-md-6"><?php dynamic_sidebar('getintouch-section'); ?></div>
-	<div id="contact-second-col" class="col-md-6"><?php dynamic_sidebar('connected-section'); ?></div>
-</div>
-
-<?php //get_footer(); ?>
- <!-- FOOTER -->
- <footer id="footer" class="<?php echo czr_fn__f('tc_footer_classes', '') ?>">
-  	<?php //do_action( '__footer' ); // hook of footer widget and colophon?>
-	<div class="row">
-		<div class="col-md-6">
-			<p>&copy; <?php echo date('Y'); ?></span> <a href="#">Keller Williams Realty Black Hills</a> &ndash; All rights reserved</p>
-		</div>
-
-	</div>
-</footer>
-<?php
-do_action( '__after_page_wrap' );
-wp_footer(); //do not remove, used by the theme and many plugins
-do_action( '__after_footer' );
-?>
-*/ ?>
-
+    <?php get_footer(); ?>
 </body>
 <?php do_action( '__after_body' ); ?>
 </html>
