1aa1d6b3c5c1d58ae0534ab5a4327bb8cf0b924c
Nick Eldred <knewedge@gmail.com>
2019-07-14 12:14:26 -0500

    updated fontawesome icons in footer

--- footer.php
+++ footer.php
@@ -7,20 +7,20 @@
              
                     <h3>Get Connected</h3>
                 <p>
-                    <a class="footer-icons" href="https://www.facebook.com/TrueNorthAssetGroup/"><i class="fab fa-facebook-square"></i></a>
-                    <a class="footer-icons" target="_blank" href="https://twitter.com/AssetTrue"><i class="fa fa-twitter fa-fw"></i></a>
-                    <a class="footer-icons" target="_blank" href="https://www.linkedin.com/company/true-north-asset-group/"><i class="fa fa-linkedin fa-fw"></i></a>
-                    <a class="footer-icons" target="_blank" href="https://www.youtube.com/channel/UCJSFGQ9bm5nQCaDHM82q3ig"><i class="fa fa-youtube fa-fw"></i></a>
-                    <a class="footer-icons" target="_blank" href="https://www.instagram.com/truenorthassetgroup/"><i class="fa fa-instagram fa-fw"></i></a>
+                    <a class="footer-icons" href="https://www.facebook.com/TrueNorthAssetGroup/"><i class="fab fa-facebook"></i></a>
+                    <a class="footer-icons" target="_blank" href="https://twitter.com/AssetTrue"><i class="fab fa-twitter"></i></a>
+                    <a class="footer-icons" target="_blank" href="https://www.linkedin.com/company/true-north-asset-group/"><i class="fab fa-linkedin"></i></a>
+                    <a class="footer-icons" target="_blank" href="https://www.youtube.com/channel/UCJSFGQ9bm5nQCaDHM82q3ig"><i class="fab fa-youtube"></i></a>
+                    <a class="footer-icons" target="_blank" href="https://www.instagram.com/truenorthassetgroup/"><i class="fab fa-instagram"></i></a>
                 
                 </p>
             </div>
             <div class="col-md-3">
                 <h3>CONTACT</h3>
-                <p><i class="fa fa-building-o"></i> Keller Williams Realty Black Hills 2401 W Main Street Rapid City, SD 57702</p>
-                <p><i class="fa fa-phone"></i> (605) 343-7500</p>
-                <p><i class="fa fa-envelope-o"></i> contact@truenorthassetgroup.com</p>
-                <p><i class="fa fa-desktop"></i> Visit Us at Our Office: 1315 Haines Avenue, Suite C Rapid City, SD 57701</p>
+                <p><i class="fas fa-building"></i> Keller Williams Realty Black Hills 2401 W Main Street Rapid City, SD 57702</p>
+                <p><i class="fas fa-phone"></i> (605) 343-7500</p>
+                <p><i class="fas fa-envelope"></i> contact@truenorthassetgroup.com</p>
+                <p><i class="fas fa-desktop"></i> Visit Us at Our Office: 1315 Haines Avenue, Suite C Rapid City, SD 57701</p>
             </div>
             <div class="col-md-2">
 		<h3>LATEST LISTINGS</h3>
