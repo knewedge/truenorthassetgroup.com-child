46171bfa95598385966274d489152b1d91908771
Nick Eldred <knewedge@gmail.com>
2019-08-12 23:11:09 -0500

    added custom search page template

--- page-search.php
+++ page-search.php
@@ -3,7 +3,7 @@
 <div id="main-wrapper" class="section">
 	<div class="row">
 		<div class="col-md-12">
-			<h3 style="display:block;width:100%;text-align:left;">Overview</h3>
+			<h2 class="listing-title"><?php the_title(); ?></h2>
 			<?php
 			global $post;
 			echo apply_filters('the_content',$post->post_content);
