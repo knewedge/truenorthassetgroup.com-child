10eff86678ef0f660b1d7c95c1f37bd9a5ac3ceb
Nick Eldred <knewedge@gmail.com>
2019-08-21 00:52:49 -0500

    converted search page template into a template

--- template-idx-search.php
+++ template-idx-search.php
@@ -6,26 +6,21 @@
 		<div class="col-md-12">
 			<h2 style="margin-bottom:2.5rem;" class="listing-title"><?php the_title(); ?></h2>
 			<?php if(strpos($_SERVER['QUERY_STRING'], "tyType=COM") > 0){ ?>
-			<a style="margin-bottom:20px;" class="btn btn-danger" href="/property-search/?search=lease" id="com-sale-btn">Search Commercial Properties For Lease</a>
+			<a style="margin-bottom:20px;" class="btn btn-danger" href="/search-lease/" id="com-sale-btn">Search Commercial Properties For Lease</a>
+			<?php }else if(strpos($_SERVER['REQUEST_URI'], "earch-lease") > 0){ ?>
+			<a style="margin-bottom:20px;" class="btn btn-danger" href="/property-search/?propertyType=COM&propertyType=LL&propertyType=RI&status=active" id="com-sale-btn">Search Commercial Properties For Sale</a>
+			<?php } ?>	
 			<?php
 			global $post;
 			echo apply_filters('the_content',$post->post_content);
 			?>
-			<?php }else if(strpos($_SERVER['QUERY_STRING'], "arch=lease") > 0){ ?>
-			<a style="margin-bottom:20px;" class="btn btn-danger" href="/property-search/?propertyType=COM&propertyType=LL&propertyType=RI&status=active" id="com-sale-btn">Search Commercial Properties For Sale</a>		
-			<?php
-			echo do_shortcode('[searchandfilter id="1052"]');
-			echo do_shortcode('[searchandfilter id="1052" action="filter_next_query"]');
-			echo do_shortcode('[post_grid id="1064"]');
-			?>
-			<?php } ?>
 		</div>
 	</div>
 </div>
 <!-- Hide & Show Search Sections Logic -->
 <style>
 a.btn-danger:hover{
-	color:#ffffff;
+	color:#ffffff!important;
 }
 </style>
 <?php get_footer(); ?>
