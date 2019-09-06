550f4463e84b2916fd9f5dfb5087a64e5c23663c
Nick Eldred <knewedge@gmail.com>
2019-09-05 21:52:09 -0500

    added bootstrap tabs to idx-search

--- template-idx-search.php
+++ template-idx-search.php
@@ -5,6 +5,11 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h2 style="margin-bottom:2.5rem;" class="listing-title"><?php the_title(); ?></h2>
+			<ul class="nav nav-tabs">
+  				<li class="nav-item"><a class="nav-link active" href="#">Active</a></li>
+  				<li class="nav-item"><a class="nav-link" href="#">Link</a></li>
+  				<li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a></li>
+			</ul>
 			<?php if(strpos($_SERVER['QUERY_STRING'], "tyType=COM") > 0){ ?>
 			<a style="margin-bottom:20px;" class="btn btn-danger" href="/search-lease/" id="com-sale-btn">Search Commercial Properties For Lease</a>
 			<?php }else if(strpos($_SERVER['REQUEST_URI'], "earch-lease") > 0){ ?>
@@ -14,11 +19,6 @@
 			global $post;
 			echo apply_filters('the_content',$post->post_content);
 			?>
-			<ul class="nav nav-tabs">
-  				<li class="nav-item"><a class="nav-link active" href="#">Active</a></li>
-  				<li class="nav-item"><a class="nav-link" href="#">Link</a></li>
-  				<li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a></li>
-			</ul>
 		</div>
 	</div>
 </div>
