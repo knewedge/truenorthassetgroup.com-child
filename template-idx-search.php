5b3c882963530695d72c39bcfd5bca7f21f4c9c6
Nick Eldred <knewedge@gmail.com>
2019-09-05 22:48:10 -0500

    added bootstrap tabs to idx-search

--- template-idx-search.php
+++ template-idx-search.php
@@ -7,13 +7,20 @@
 			<h2 style="margin-bottom:2.5rem;" class="listing-title"><?php the_title(); ?></h2>
 			<ul class="nav nav-tabs">
 			<?php if(strpos($_SERVER['QUERY_STRING'], "tyType=COM") > 0){ ?>
-			<li class="nav-item"><a class="nav-link active" href="#">For Sale</a></li>
-  			<li class="nav-item"><a class="nav-link disabled" href="/search-lease/">For Lease</a></li>
+			<h2 style="margin-bottom:2.5rem;" class="listing-title"><?php the_title(); ?> - Commercial</h2>
+			<ul class="nav nav-tabs">
+				<li class="nav-item"><a class="nav-link active" href="#">For Sale</a></li>
+				<li class="nav-item"><a class="nav-link disabled" href="/search-lease/">For Lease</a></li>
+			</ul>
 			<?php }else if(strpos($_SERVER['REQUEST_URI'], "earch-lease") > 0){ ?>
-			<li class="nav-item"><a class="nav-link disabled" href="/property-search/?propertyType=COM&propertyType=LL&propertyType=RI&status=active">For Sale</a></li>
-  			<li class="nav-item"><a class="nav-link active" href="#">For Lease</a></li>
-			<?php } ?>
-			</ul>	
+			<h2 style="margin-bottom:2.5rem;" class="listing-title"><?php the_title(); ?> - Commercial</h2>
+			<ul class="nav nav-tabs">	
+				<li class="nav-item"><a class="nav-link disabled" href="/property-search/?propertyType=COM&propertyType=LL&propertyType=RI&status=active">For Sale</a></li>
+  				<li class="nav-item"><a class="nav-link active" href="#">For Lease</a></li>
+			</ul>
+			<?php }else{ ?>
+			<h2 style="margin-bottom:2.5rem;" class="listing-title"><?php the_title(); ?></h2>
+			<?php } ?>	
 			<?php
 			global $post;
 			echo apply_filters('the_content',$post->post_content);
